82baf84f852ae52cfadc8311e5faf5b42495be38
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-17 09:07:06 -0400

    initialcommit

---
+++ homework.js
@@ -0,0 +1,59 @@
+// 1.
+
+// function maxOfTwoNumbers(num1, num2){
+//     if(num1 > num2){
+//         return num1;
+//     }else if(num1 < num2){
+//         return num2;
+//     }else{
+//         return num1;
+//     }
+// }
+
+// console.log(maxOfTwoNumbers(-6,5));
+
+
+
+
+
+// 2.
+
+// const maxOfThree = function (num1,num2,num3){
+//     if(num1 >= num2 && num1 >= num3){
+//         return num1;
+//     }else if(num2 >= num1 && num2 >= num3){
+//         return num2;
+//     }else if(num3 >= num1 && num2 >= num3){
+//         return num3;
+//     }
+// }
+
+// console.log(maxOfThree(22,23,23))
+
+
+
+
+// 3.
+
+// function charAVowel(vowel){
+//     if(vowel === 'a' || vowel === 'e' || vowel === 'i' || vowel === 'o' || vowel === 'u' || vowel === 'A' || vowel === 'E' || vowel === 'I' || vowel === 'O' || vowel === 'U'){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// console.log(charAVowel('A'))
+
+
+// 4.
+
+
+// let sum = 0;
+// let sumArray = function(numbers = []){
+//      numbers.forEach(element => {
+//       sum += element;  
+//     });;
+// return sum;
+// }
+// console.log(sumArray([1,2,2,3,4,5]))
